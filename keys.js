33496fe54048b618e9f10ecbb2895b5962c489dc
Kumar Gaurav <61309195+ningenda@users.noreply.github.com>
2020-08-08 08:47:55 +0530

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID: '88780693579-34i4a3jfq0q15eg7kn10dbqf6rpu9if8.apps.googleusercontent.com',
+  googleClientSecret: 'kl8zUPpOSWvan4vJjOoaiJL5',
+  mongoURI : 'mongodb+srv://ningen:<UKMtwh6WuJTxZKMK>@emaily.xtsm0.mongodb.net/<emaily11>?retryWrites=true&w=majority'
+};
